c7c6219c525d9691f8c155f710adadfa00314aab
delauraen <22270079+delauraen@users.noreply.github.com>
2024-08-19 14:45:48 -0700

    added more loot
    tier2 and tier3 loot options that give more materials, plus added planets to parallax

--- Scripts/Background.gd
+++ Scripts/Background.gd
@@ -1,11 +1,14 @@
 extends ParallaxBackground
 
 @onready var back_layer = $BackLayer
+@onready var planet_layer = $PlanetLayer
 @onready var front_layer = $FrontLayer
 @onready var mid_layer = $MidLayer
 
 
 func _process(delta):
+	planet_layer.motion_offset.y += 2*delta
+	planet_layer.motion_offset.y += 2*delta
 	mid_layer.motion_offset.y += 5*delta
 	mid_layer.motion_offset.x += 5*delta
 	front_layer.motion_offset.y += 10*delta
